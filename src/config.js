de6a3975c18cd46205ecce705afb8a5d848e4b4e
fuyong <fuyong1325@163.com>
2022-06-09 18:02:32 +0800

    修改打开程序显示登录页

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // 连本地
-  BASE_URL: 'http://192.168.0.77:8143',
+  BASE_URL: 'http://192.168.0.77:8143/user/login',
   
   // 连测试
-  // BASE_URL: '//192.168.0.77:8143',
+  // BASE_URL: '//192.168.0.77:8143/user/login',
   
   // 连正式
-  // BASE_URL: '//192.168.0.58:8143',
+  // BASE_URL: '//192.168.0.58:8143/user/login',
 }
