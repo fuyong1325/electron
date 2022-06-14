f512fb719ce1c996f584d6a0dc1cad1c05fec18f
Louis Loong <df74@foxmail.com>
2022-06-14 13:51:22 +0800

    更新测试环境、正式环境域名

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ module.exports = {
   BASE_URL: 'http://192.168.0.77:8143/user/login',
   
   // 连测试
-  // BASE_URL: '//192.168.0.77:8143/user/login',
+  // BASE_URL: 'https://test-org-pc.ebi-geriatrics.com/user/login',
   
   // 连正式
-  // BASE_URL: '//192.168.0.58:8143/user/login',
+  // BASE_URL: 'https://www.ebi-geriatrics.com/user/login',
 }
