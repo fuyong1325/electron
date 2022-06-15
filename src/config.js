9c4a32a3aa149fb05593deb3db8340394a3a77e0
fuyong <fuyong1325@163.com>
2022-06-15 14:31:03 +0800

    修改内部版本号

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 内部版本号
-  version: 2,
+  version: 1,
   // 连本地
   // BASE_URL: 'http://192.168.0.77:8143/user/login',
   UPLOAD_URL: 'https://ebicom-public.ebi-geriatrics.com/public/app/gebm-win',
